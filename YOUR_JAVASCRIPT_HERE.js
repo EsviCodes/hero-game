7566b719493773da8f662c502e6103bd819d7d77
EsviCodes <esther.js.vis@gmail.com>
2019-09-21 07:46:20 +0200

    Created the HERO Object

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,15 @@
+// Write your JS here
+
+// YAY - Let's do this! :-)
+
+// HERO OBJECT
+const hero = {
+  name: "Wonder Woman",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "sword",
+    damage: 2
+  }
+};
